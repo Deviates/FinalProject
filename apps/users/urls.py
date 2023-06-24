72bc73dd354bfabe96de19d433424e7532c9c700
Abdulloh17 <albybarnes17@gmail.com>
2023-06-24 23:05:05 +0600

    new

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView  
-from apps.users.views import register, user_login, user_account, my_ajax_view
+from apps.users.views import register, user_login, user_account
 
 
 urlpatterns = [
     path('register/', register, name="register"),
     path('login/', user_login, name="login"),
     path('user/<int:id>/', user_account, name="user_account"),
-    path('my-ajax-view/', my_ajax_view, name='my-ajax-view'),
     path('logout/', LogoutView.as_view(next_page = "index"), name = "logout"),
 ]
