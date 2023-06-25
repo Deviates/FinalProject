cbd1f3118c646b064e42e5a5b5d70ce77ceaf030
Simon070805 <abdykadyrovsyimyk0708@gmail.com>
2023-06-25 12:54:33 +0600

    added the ability to send anonymous messages

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 
-from apps.settings.models import Setting, Benefit,Contact,About,Data
+from apps.settings.models import Setting, Benefit,Contact,About,Data, Anonym_messages
 from apps.courses.models import Category, Courses
 from apps.telegram.views import get_text
 from apps.users import views
@@ -12,12 +12,35 @@ def index(request):
     categories = Category.objects.all().order_by('?')
     courses = Courses.objects.all()
     benefits = Benefit.objects.all().order_by('?')
+    if request.method == "POST":
+        name2 = request.POST.get("name2")
+        message2 = request.POST.get("message2")
+
+        reviews = Anonym_messages.objects.create(name2 = name2, message2 = message2)
+
+        get_text(f""" Оставлен анонимный отзыв
+Кому: {reviews.name2}
+Текст: {reviews.message2}
+""")
+    context = {
+        'about' : about,
+    }
     return render(request,'basic/index.html',locals())
 
 def about(request):
     setting = Setting.objects.latest('id')
     about = About.objects.latest('id')
     data = Data.objects.latest('id')
+    if request.method == "POST":
+        name2 = request.POST.get("name2")
+        message2 = request.POST.get("message2")
+
+        reviews = Anonym_messages.objects.create(name2 = name2, message2 = message2)
+
+        get_text(f""" Оставлен анонимный отзыв
+Кому: {reviews.name2}
+Текст: {reviews.message2}
+""")
     return render(request,'basic/about.html',locals())
 
 def contacts(request):
--- apps/teachers/views.py
+++ apps/teachers/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .models import Teachers
 from apps.courses.models import Courses
-from apps.settings.models import Setting
+from apps.settings.models import Setting, Anonym_messages
+from apps.telegram.views import get_text
 
 
 # Create your views here.
 def teachers(request):
     settings = Setting.objects.latest("id")
     teachers = Teachers.objects.all()
+    if request.method == "POST":
+        name2 = request.POST.get("name2")
+        message2 = request.POST.get("message2")
+
+        reviews = Anonym_messages.objects.create(name2 = name2, message2 = message2)
+
+        get_text(f""" Оставлен анонимный отзыв
+Кому: {reviews.name2}
+Текст: {reviews.message2}
+""")
     
 
     context = {
@@ -21,6 +32,16 @@ def detail_teacher(request, id):
     teacher = Teachers.objects.get(id=id)
     course = Courses.objects.all()
     teachers = Teachers.objects.all()
+    if request.method == "POST":
+        name2 = request.POST.get("name2")
+        message2 = request.POST.get("message2")
+
+        reviews = Anonym_messages.objects.create(name2 = name2, message2 = message2)
+
+        get_text(f""" Оставлен анонимный отзыв
+Кому: {reviews.name2}
+Текст: {reviews.message2}
+""")
 
     
     return render(request, "single-teacher.html" , locals())
