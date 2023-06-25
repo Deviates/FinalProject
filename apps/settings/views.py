367b64ec7d901d6f2216f5b0449d65e087e728d5
Simon070805 <abdykadyrovsyimyk0708@gmail.com>
2023-06-25 11:37:13 +0600

    change telegram

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 
 from apps.settings.models import Setting, Benefit,Contact,About,Data
 from apps.courses.models import Category, Courses
+from apps.telegram.views import get_text
 
 # Create your views here.
 def index(request):
@@ -24,6 +25,15 @@ def contacts(request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
+
+        review = Contact.objects.create(name = name, email = email, message = message)
+
+        get_text(f""" Оставлен отзыв 
+Имя пользователя: {review.name}
+Адрес(email): {review.email}
+Текст: {review.message}
+""")
+
         Contact.objects.create(name = name,email = email,message = message)
         send_mail(
             f'{message}',
